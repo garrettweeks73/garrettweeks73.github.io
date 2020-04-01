2536cc282fad4c9d4717abd62bdc7dcd64d7c6c6
garrettweeks73 <gweeks@electradigital.com>
2020-04-01 08:30:01 -0500

    js file

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#menu li a").on('click', function(e) {
+        e.preventDefault();
+        var page = $(this).data('page');
+        $("#pages .page:not('.hide')").stop().fadeOut('fast', function() {
+            $(this).addClass('hide');
+          $('#pages .page[data-page="'+page+'"]').fadeIn('slow').removeClass('hide');
+      })
+  })
+})
